250627a72a92122b8b85e84222b22ec1064d12fe
Charl913 <charlkruger61@gmail.com>
2023-09-27 12:14:30 +0200

    Create lists with predefined data

--- ListsExercise/ListsExercise/Airbnb.xaml.cs
+++ ListsExercise/ListsExercise/Airbnb.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ListsExercise.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,24 @@ namespace ListsExercise
         public Airbnb()
         {
             InitializeComponent();
+
+            listView.ItemsSource = new List<Search> 
+            {
+                new Search 
+                {
+                    Id = 1,
+                    Location = "West Hollywood, CA, United States",
+                    CheckIn = new DateTime(2016, 9, 1),
+                    CheckOut = new DateTime(2016, 11, 1)
+                },
+                new Search
+                {
+                    Id = 2,
+                    Location = "Santa Monica, CA, United States",
+                    CheckIn = new DateTime(2016, 9, 1),
+                    CheckOut = new DateTime(2016, 11, 1)
+                }
+            };
         }
     }
 }
